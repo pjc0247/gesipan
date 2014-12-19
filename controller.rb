08a7ae805e266133fd6cf8c4af1375770b299295
Park June Chul <pjc0247@naver.com>
2014-12-19 11:35:59 +0900

    initial

---
+++ controller.rb
@@ -0,0 +1,52 @@
+﻿require 'sinatra'
+require 'data_mapper'
+require 'digest'
+load 'models.rb'
+
+# VIEW ROUTES
+get '/' do
+  erb :write_post
+end
+
+get '/register_account' do
+  erb :register_account
+end
+
+get '/write_post' do
+  erb :write_post
+end
+post '/write_comment' do
+  erb :write_comment
+end
+get '/list' do 
+  @posts = Post.all
+
+  erb :list
+end
+
+# BACKEND CONTROLLERS
+post '/account/new' do
+  account = Account.create params
+  
+  redirect '/list'
+end
+
+post '/post/like' do 
+  post = Post.get params[:post_id]
+  post.likes.create
+
+  redirect '/list'
+end
+post '/post/new' do
+  Post.create params
+
+  redirect '/list'
+end
+post '/post/comment/new' do 
+  post = Post.get params[:post_id]
+  post.comments.create(
+    :body => params[:body])
+
+  redirect '/list'
+end
+
---
+++ models.rb
@@ -0,0 +1,59 @@
+require 'rubygems'
+require 'data_mapper'
+require 'dm-migrations'
+
+DataMapper::Logger.new($stdout, :debug)
+DataMapper.setup(:default, 'sqlite::memory:')
+
+class Account
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :user_id, String, :required => true
+  property :user_pw, String, :required => true
+  property :nickname, String, :required => true
+  property :created_at, DateTime
+
+  validates_length_of :user_id, :max => 32
+  validates_length_of :user_pw, :max => 32
+  validates_length_of :nickname, :max => 16
+
+#  has n, :posts
+  has n, :comments
+end
+
+class Post
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :title, String, :required => true
+  property :body, Text, :required => true
+  property :created_at, DateTime
+
+#  belongs_to :user
+  has n, :comments
+  has n, :likes
+end
+class Comment
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :body, Text, :required => true
+  property :created_at, DateTime
+
+  belongs_to :account
+  belongs_to :post
+end
+
+class Like
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :created_at, DateTime
+
+  belongs_to :account
+  belongs_to :post
+end
+
+DataMapper.finalize
+DataMapper.auto_migrate!
